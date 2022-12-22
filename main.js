0757387b7633cd070bbfb4a03b30e967e28f2c17
Gibaa <gilberto.alves.jr@gmail.com>
2022-12-21 23:10:37 -0300

    adding functions

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 const randomNumber = Math.round(Math.random() * 10);
 let attempts = 1;
+console.log(randomNumber)
 
 
 function handleClick(event) {
     event.preventDefault()
 
-    const inputNumber = document.querySelector("#inputNumber").value
+    const inputNumber = document.querySelector("#inputNumber")
 
-    attempts++
-    console.log(randomNumber)
-
-    if(Number(inputNumber) == randomNumber) {
+    if(Number(inputNumber.value) == randomNumber) {
         document.querySelector(".screen1").classList.add("hide")
         document.querySelector(".screen2").classList.remove("hide")
+        if (attempts == 1) {
 
-        console.log(`Você acertou em ${attempts} tentativas`)
+            document.querySelector(".screen2 h2").innerText = `Você acertou em ${attempts} tentativa!`
+        } else {
+            
+            document.querySelector(".screen2 h2").innerText = `Você acertou em ${attempts} tentativas!`
+        }
     }
 
+    attempts++
 }
+
+//eventos
+
+btnTry = document.querySelector("#btnTry")
+btnReset = document.querySelector("#btnReset")
